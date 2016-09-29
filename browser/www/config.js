fb12587ed8050686c3017f9ce1afabad706d6867
Ngo Hoai Phuong <phuongnh@vinasource.com>
2016-09-29 14:34:48 +0700

    create master branch

--- browser/www/config.js
+++
@@ -1,3 +0,0 @@
-APPLICATIONID='C5E7FSUJXEJ7gTODpBLCuZShhJUxhu9AHjdlU4QR-AwsProduction'
-SERVERURL='https://server.parentplanet.com:13370/parse/'
-MODESERVER='Production'
--- browser/www/js/dataStorage.js
+++
@@ -1,7 +0,0 @@
-define("SomeCollection", ["localstorage"], function() {
-    var SomeCollection = Backbone.Collection.extend({
-        localStorage: new Backbone.LocalStorage("SomeCollection") // Unique name within your app.
-    });
-
-    return SomeCollection;
-});
