d2c094833241b19316e4785b5bc61a16dc825912
Ngo Hoai Phuong <phuongnh@vinasource.com>
2016-10-06 15:46:38 +0700

    edit

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
   // echo getcwd() . "<br>";
-  // chdir('./Cordova_v4/ParentPlanet/platforms/browser/www');
+  chdir('./Cordova_v4/ParentPlanet/platforms/browser/www');
   echo getcwd() . "<br>";
   define("ROOT", "Cordova_v4/ParentPlanet/platforms/browser/www");
   echo getcwd() . "<br>";
